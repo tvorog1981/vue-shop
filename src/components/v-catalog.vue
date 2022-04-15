02906f1f6217a11d8d500f4648f0d7e963fa3be3
Tvorog1981 <smetankin.denis@yandex.ru>
2022-04-15 16:56:23 +0300

    create projetct step 3

---
+++ src/components/v-catalog.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="v-catalog">
+           Catalog
+    </div>
+</template>
+
+
+<script>
+
+
+export default {
+  
+}
+</script>
+<style>
+.v-catalog{
+
+}
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="v-main-wrapper">
+
+    </div>
+</template>
+
+
+<script>
+
+
+export default {
+  
+}
+</script>
+<style>
+.v-main-wrapper{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width:  900px;
+    margin: 0 auto;
+}
+</style>
